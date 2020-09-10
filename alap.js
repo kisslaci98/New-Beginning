33e2644e36a77193c2ff4df20a3757c45b8b6662
kisslaci98 <klaci9898@gmail.com>
2020-09-10 09:29:58 +0200

    Harmadik commit

---
+++ alap.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let nev = 'Kiss László';
+
+/*Modális ablak függvények*/ 
+//window.alert(nev);
+//alert(nev);
+
+/*let tanuloszam = window.prompt('Hány tanuló van jelen: ');
+let szoveg = `A tanulók száma: ${tanuloszam}`;
+window.alert(szoveg);*/
+
+let szoveg = 'Hello World!';
+//szoveg = null;
+//window.alert(szoveg);
+
+document.getElementById('kiir').innerText = szoveg;
